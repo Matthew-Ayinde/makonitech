442d28c52eddb3f762aa7a30714279a05a0bd894
Elijah Obominuru <elijahobominuru@gmail.com>
2021-10-04 08:25:26 +0100

    styled table component across

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,29 +1,23 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 import { Container } from "react-bootstrap";
-import Herobg from "../images/hero-bg.png"
+//import Herobg from "../images/hero-bg.png"
 
 const Hero = () => {
 
   return (
     <section className="hero">
       <Container>
-        <div className="hero_bg">
-          <img src={Herobg} alt="Hero" />
-        </div>
         <div className="hero_content">
           <div className="hero_info">
             <h1>
-              <span>Building secure</span>
-              <br /> 
-              <span>on-prem & cloud</span> 
-              <br />
-              <span>environment.</span>
+              Building secure
+              on-prem & cloud
+              environment.
             </h1>
             <p>
-              <span>Defending your users and data, no matter where they are,</span> 
-              <br />
-              <span>is a more critical job than ever before.</span>
+              Defending your users and data, no matter where they are,
+              is a more critical job than ever before.
             </p>
           </div>
           <div className="hero_nav">
--- src/components/Reason.js
+++ src/components/Reason.js
@@ -15,33 +15,36 @@ const Reason = () => {
           We don’t want our brand associated with anything that isn’t of the higest quality. We make quality products, we 
         give quality advice and we only hire qulity people. People who want the best come to us for a reason
         </p>
-        <section className="reasons_inner">
-          <div>
-            <img src={nuts} alt="" />
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="51" height="16" viewBox="0 0 51 16">
-              <path fill="#040404" d="M50.7 8.7a1 1 0 0 0 0-1.4L44.35.92a1 1 0 1 0-1.41 1.41L48.59 8l-5.66 5.66a1 1 0 0 0 1.41 1.41l6.37-6.36ZM0 9h50V7H0v2Z"/>
-            </svg>
-            <h4>Our Story</h4>
-          </div>
-        </section>
-        <div className="inner_vert"></div>
-        <section className="reasons_inner">
-          <div>
-            <img src={rotate} alt="" />
-            <span className="inner_line"></span>
-            <h4>Our Services</h4>
-          </div>
-        </section>
-        <div className="inner_vert"></div>
-        <section className="reasons_inner">
-          <div>
-            <img src={cart} alt="" />
-            <span className="inner_line"></span>
-            <h4>Industries we serve</h4>
-          </div>
-        </section>
+        <div className="reasons_option">
+          <section className="reasons_inner">
+            <div>
+              <img src={nuts} alt="" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="51" height="16" viewBox="0 0 51 16">
+                <path fill="#040404" d="M50.7 8.7a1 1 0 0 0 0-1.4L44.35.92a1 1 0 1 0-1.41 1.41L48.59 8l-5.66 5.66a1 1 0 0 0 1.41 1.41l6.37-6.36ZM0 9h50V7H0v2Z"/>
+              </svg>
+              <h4>Our Story</h4>
+            </div>
+          </section>
+          <div className="inner_vert"></div>
+          <section className="reasons_inner">
+            <div>
+              <img src={rotate} alt="" />
+              <span className="inner_line"></span>
+              <h4>Our Services</h4>
+            </div>
+          </section>
+          <div className="inner_vert"></div>
+          <section className="reasons_inner">
+            <div>
+              <img src={cart} alt="" />
+              <span className="inner_line"></span>
+              <h4>Industries we serve</h4>
+            </div>
+          </section>
+        </div>
         
       </section>
+      
       <section className="reason_list">
         {
           reasonData.map(reas => {
