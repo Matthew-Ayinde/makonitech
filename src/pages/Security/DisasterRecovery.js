77cfebfc33320464af852e467af89723fc957d00
Princess-Jewel <princess.jel-edema@cloudfift.com>
2021-11-17 22:56:54 +0100

    created all the pages for the navbar

---
+++ src/pages/Security/DisasterRecovery.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Help from "../../components/Help/Help";
+import ServicesHero from "../../components/Hero/ServicesHero";
+import ServicesTab from "../../components/Tab/ServicesTab";
+import Results from "../../components/Results/Results";
+import AdvertOne from "../../components/Adverts/AvertOne";
+import MakoniWay from "../../components/MakoniWay/MakoniWay";
+import Insight from "../../components/Insight";
+import OurWork from "../../components/Work/OurWork";
+
+const DisasterRecovery = () => {
+  return (
+    <>
+      <ServicesHero title="Disaster Recovery" category="Security" />
+      <ServicesTab />
+      <Results />
+      <OurWork />
+      <Help />
+      <AdvertOne />
+      <MakoniWay />
+      <Insight />
+    </>
+  );
+};
+export default DisasterRecovery ;
